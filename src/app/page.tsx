2ffa6988cd8cd3b2609c312843f82217844ff8be
Neeraj Dalal <admin@nrjdalal.com>
2023-11-30 14:22:45 +0530

    v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,67 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
-export default async function Page() {
+const Page = () => {
   return (
-    <main className="flex flex-col items-center p-5">
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-          <Link
-            href="https://twitter.com/nrjdalal_com"
-            className="bg-muted rounded-2xl border bg-slate-100 px-4 py-1.5 text-sm font-medium"
-            target="_blank"
-          >
-            Follow along on Twitter
-          </Link>
-          <h1 className="font-heading text-3xl sm:text-5xl">
-            An open source Next.js bare starter.
-            <br />
-            With step-by-step instructions if required.
-          </h1>
-          <p className="text-muted-foreground max-w-[42rem] leading-normal sm:text-xl sm:leading-8">
-            Onset is a Next.js starter that comes with step-by-step instructions
-            to understand how everything works, easy for both beginners and
-            experts alike and giving you the confidence to customize it to your
-            needs. Built with Next.js 14, Drizzle (Postgres), NextAuth/Auth.js.
-          </p>
-          <div className="mt-4 space-x-4">
+    <main className="container mx-auto max-w-screen-xl text-slate-800">
+      {
+        // ~ Header
+      }
+      <div className="mx-5 flex h-12 items-center">
+        <p className="text-md font-semibold">NEERAJ DALAL</p>
+      </div>
+
+      {
+        // ~ About
+      }
+      <div className="mx-2.5 flex flex-col justify-center border-t border-slate-800 px-2.5 py-20 font-medium md:py-60">
+        <p className="text-2xl text-amber-600 md:text-3xl">
+          Dream. Sometimes you got to close your eyes and envision the future.
+        </p>
+
+        <p className="pt-10 text-2xl md:text-3xl">
+          I&apos;m Neeraj Dalal, a software engineer based in New Delhi, India.
+        </p>
+
+        <p className="pt-2 text-2xl md:text-3xl">
+          And I will help you to turn your vision into reality.
+        </p>
+      </div>
+
+      {
+        // ~ Skills
+      }
+      <div className="mx-5 flex flex-col border-t border-slate-300 lg:py-10">
+        <div className="grid gap-x-16 gap-y-12 py-20 lg:grid-cols-2">
+          <div className="overflow-hidden rounded-lg border border-slate-300">
+            <div className="bg-slate flex h-7 w-full items-center gap-x-2 px-3">
+              <div className="h-3.5 w-3.5 rounded-full bg-red-400" />
+              <div className="h-3.5 w-3.5 rounded-full bg-yellow-400" />
+              <div className="h-3.5 w-3.5 rounded-full bg-green-400" />
+            </div>
+            <Image src="/rdt.li.png" alt="Next.js" width={1080} height={1920} />
+          </div>
+
+          <div className="flex flex-col justify-center font-medium">
+            <h2 className="text-2xl md:text-3xl">Redirect Link</h2>
+
+            <p className="pt-8 text-xl text-slate-500">
+              rdt.li self hostable, feature rich, minimalistic and open source
+              URL shortener. Built with Next.js, Drizzle, NextAuth and Postgres.
+            </p>
+
             <Link
-              href="/access"
-              className="rounded-md bg-slate-900 px-8 py-2.5 text-white"
-            >
-              Login
-            </Link>
-            <Link
-              href="https://github.com/nrjdalal/onset"
+              href={'https://rdt.li'}
               target="_blank"
-              rel="noreferrer"
-              className="rounded-md border px-8 py-2.5"
+              className="mt-8 w-max rounded-xl border-2 border-amber-600 px-4 py-2 text-amber-600"
             >
-              GitHub
+              Visit Website
             </Link>
           </div>
         </div>
-      </section>
-
-      <section id="open-source" className="container py-8 md:py-12 lg:py-24">
-        <div className="mx-auto flex max-w-[58rem] flex-col items-center justify-center gap-4 text-center">
-          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl">
-            Proudly Open Source
-          </h2>
-          <p className="text-muted-foreground max-w-[85%] leading-normal sm:text-lg sm:leading-7">
-            Onset is open source and powered by open source software. <br /> The
-            code is available on{' '}
-            <Link
-              href="https://github.com/nrjdalal/onset"
-              target="_blank"
-              rel="noreferrer"
-              className="underline underline-offset-4"
-            >
-              GitHub
-            </Link>
-            .{' '}
-          </p>
-        </div>
-      </section>
+      </div>
     </main>
   )
 }
+
+export default Page
